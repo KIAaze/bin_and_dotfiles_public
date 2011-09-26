3f5cac33b56429fa32690399402656899a712159
KIAaze <zohn.joidberg@gmail.com>
2011-09-26 12:18:11 +0100

    different ways of doing things

--- bins/public_bin/md5_password_protect_example.sh
+++ bins/public_bin/md5_password_protect_example.sh
@@ -5,14 +5,26 @@ set -eux
 #$  echo hello | md5sum
 #b1946ac92492d2347c6235b4d2611184  -
 
+#$ echo -n hello | md5sum
+#5d41402abc4b2a76b9719d911017c592  -
+
+#$ md5sum <<< hello
+#b1946ac92492d2347c6235b4d2611184  -
+
 echo "password"
 read x;
 #rm -iv /tmp/tmpfifo;
 #mkfifo /tmp/tmpfifo;
 #echo $x > tmpfifo;
-echo $x | md5sum
-md5=$(echo $x | md5sum | awk '{print $1;}');
-if [ $md5 == "b1946ac92492d2347c6235b4d2611184" ]
+
+#echo $x | md5sum
+#echo -n $x | md5sum
+#md5=$(echo md5sum <<< $x)
+#md5=$(echo $x | md5sum | awk '{print $1;}');
+md5=$(echo -n $x | md5sum | awk '{print $1;}');
+
+if [ $md5 == "5d41402abc4b2a76b9719d911017c592" ]
+#if [ $md5 == "b1946ac92492d2347c6235b4d2611184" ]
 then
 	echo SUCCESS
 else
