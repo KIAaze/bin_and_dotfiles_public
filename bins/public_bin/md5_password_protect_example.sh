828e2d3d68caed93f1b8daf6030bb97b66a198c8
KIAaze <zohn.joidberg@gmail.com>
2011-09-26 12:06:51 +0100

    just an md5 protection example for reference

---
+++ bins/public_bin/md5_password_protect_example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eux
+
+#getting hash from password
+#$  echo hello | md5sum
+#b1946ac92492d2347c6235b4d2611184  -
+
+echo "password"
+read x;
+#rm -iv /tmp/tmpfifo;
+#mkfifo /tmp/tmpfifo;
+#echo $x > tmpfifo;
+echo $x | md5sum
+md5=$(echo $x | md5sum | awk '{print $1;}');
+if [ $md5 == "b1946ac92492d2347c6235b4d2611184" ]
+then
+	echo SUCCESS
+else
+	echo FAILURE
+fi
+
