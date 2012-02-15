9633d0cc788fbb2b4ead7556b0ee4d40278ca9b3
KIAaze <zohn.joidberg@gmail.com>
2012-02-15 16:31:54 +0000

    -improved backup.sh by adding move option

--- bins/public_bin/backup.sh
+++ bins/public_bin/backup.sh
@@ -6,9 +6,17 @@ set -e -u
 if [ $# -lt 1 ]
 then
         echo "usage :"
-        echo "`basename $0` FILE/DIR"
+        echo "`basename MOVE? $0` FILE/DIR"
+	echo "MOVE=0 => use cp -riv"
+	echo "MOVE=1 => use mv -iv"
         exit 0
 fi
+
+MOVE=$1
+
+shift
+
+#echo $@
 
 for file in "$@"
 do
@@ -17,6 +25,15 @@ do
     FILE=$(basename "$FULLPATH")
     DATE=$(date +%Y%m%d_%H%M%S)
     NEWNAME="$FULLPATH.$DATE"
-    echo "cp -riv $FULLPATH $NEWNAME"
-    cp -riv "$FULLPATH" "$NEWNAME"
+    if [ $MOVE = "0" ]
+    then
+      echo "cp -riv $FULLPATH $NEWNAME"
+      cp -riv "$FULLPATH" "$NEWNAME"
+    elif [ $MOVE = "1" ]
+    then
+      echo "mv -iv $FULLPATH $NEWNAME"
+      mv -iv "$FULLPATH" "$NEWNAME"
+    else
+      echo "invalid MOVE argument"
+    fi
 done
