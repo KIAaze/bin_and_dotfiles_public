130d88794d74c14efbf5dec358acf8b78e27bb07
KIAaze <zohn.joidberg@gmail.com>
2012-10-04 22:13:36 +0100

    more cleanup

--- bins/public_bin/absolute_path.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Assume parameter passed in is an absolute path to a directory.
-
-# For brevity, we won't do argument type or length checking.
-
-#NOTE: Just use readlink -f $1 ...
-echo "Absolute path: `cd $1; pwd`"
