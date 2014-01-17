cbd6cf2e19dc9281ef8db4947c2efbf8e5573218
KIAaze <zohn.joidberg@gmail.com>
2014-01-17 20:46:04 +0000

    added DIR argument

--- bins/public_bin/progress_nfiles.sh
+++ bins/public_bin/progress_nfiles.sh
@@ -2,23 +2,24 @@
 
 set -u
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
   echo "usage :"
-  echo "`basename $0` NFILES_MIN NFILES_MAX"
-  echo "Checks the number of files in the current folder at regular intervals and shows a corresponding progress bar going from NFILES_MIN to NFILES_MAX."
+  echo "`basename $0` NFILES_MIN NFILES_MAX DIR"
+  echo "Checks the number of files in the directory DIR at regular intervals and shows a corresponding progress bar going from NFILES_MIN to NFILES_MAX."
   exit 0
 fi
 
+DIR=$3
 NFILES_MIN=$1
-NFILES=$(ls -1 | wc -l)
+NFILES=$(ls -1 "$DIR" | wc -l)
 NFILES_MAX=$2
 
 (
   while test $NFILES -ne $NFILES_MAX;
   do
     sleep 1s
-    NFILES=$(ls -1 | wc -l)
+    NFILES=$(ls -1 "$DIR" | wc -l)
     echo $(echo "scale=3; 100*($NFILES-$NFILES_MIN)/($NFILES_MAX-$NFILES_MIN)" | bc);
   done
 ) | zenity --progress --auto-close
