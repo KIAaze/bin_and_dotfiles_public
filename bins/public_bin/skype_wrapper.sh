9e57f7416095f288599aa76c9ab0cd89e265b06c
KIAaze <zohn.joidberg@gmail.com>
2014-10-05 11:42:55 +0100

    -setSkypeMoodText script improved (now works with py2.7, py2.6 might be broken). -Added skype start wrapper to set mood text from an environment variable.

---
+++ bins/public_bin/skype_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+if [ -f ~/.bash_env ]; then
+  source ~/.bash_env
+fi
+
+if [ -f ~/.bash_local ]; then
+  source ~/.bash_local
+fi
+
+LOGFILE="$HOME/log/skype_wrapper.log"
+
+echo "===" 1>> $LOGFILE 2>&1
+date 1>> $LOGFILE 2>&1
+echo "PATH = $PATH" 1>> $LOGFILE 2>&1
+
+skype &
+echo "SKYPE_MOOD_TEXT = ${SKYPE_MOOD_TEXT}" 1>> $LOGFILE 2>&1
+setSkypeMoodText.py2.py --waitForSkype --mood_text=${SKYPE_MOOD_TEXT} 1>> $LOGFILE 2>&1
