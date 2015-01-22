964211fbcf5efd0fac7f8cc41d66b56345d5196e
KIAaze <zohn.joidberg@gmail.com>
2015-01-22 16:49:10 +0000

    single-line version

--- bins/public_bin/ssh-get-fingerprints.sh
+++ bins/public_bin/ssh-get-fingerprints.sh
@@ -3,6 +3,9 @@
 # print out server fingerprints
 # cf http://www.phcomp.co.uk/Tutorials/Unix-And-Linux/ssh-check-server-fingerprint.html
 
+# single-line version:
+# for f in /etc/ssh/*.pub; do ssh-keygen -l -f $f; done
+
 for f in /etc/ssh/*.pub
 do
   ssh-keygen -l -f $f
