b398863a6dbc7d4c0a98fd7a1dbb8633e9ea6ab7
KIAaze <zohn.joidberg@gmail.com>
2018-06-05 18:36:54 +0100

    quick hacks...

--- bins/public_bin/autocrop.sh
+++ bins/public_bin/autocrop.sh
@@ -17,5 +17,13 @@ do
     #fi
   #fi
   convert -trim +repage "${infile}" "${outfile}"
-  convert -resize 383x693 -density 120x120 -units pixelspercentimeter "${outfile}" "${filename}.cropped.resized.${extension}"
+  
+  # [3.1917, 5.7750] cm
+  #convert -resize 383x693 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 383x693 "${outfile}" "${filename}.cropped.3.1917x5.7750.${extension}"
+  # [4, 4] cm
+  #convert -resize 480x480 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 480x480 "${outfile}" "${filename}.cropped.4x4.${extension}"
+  # 4.375x4.375 cm
+  convert -resize 525x525 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 525x525 "${outfile}" "${filename}.cropped.4.375x4.375.${extension}"
+  # [5, 5] cm
+  #convert -resize 600x600 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 600x600 "${outfile}" "${filename}.cropped.5x5.${extension}"
 done
