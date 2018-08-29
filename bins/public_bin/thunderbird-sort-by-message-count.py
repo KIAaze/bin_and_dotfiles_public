3e9950b7a6f0a489d943ece98992d9b98927fdb9
KIAaze <zohn.joidberg@gmail.com>
2018-08-29 07:16:55 +0100

    debugging

--- bins/public_bin/thunderbird-sort-by-message-count.py
+++ bins/public_bin/thunderbird-sort-by-message-count.py
@@ -31,6 +31,33 @@ import warnings
 #   https://stackoverflow.com/questions/953561/check-unread-count-of-gmail-messages-with-python
 #   https://stackoverflow.com/questions/12042724/securely-storing-passwords-for-use-in-python-script
 #   https://stackoverflow.com/questions/7014953/i-need-to-securely-store-a-username-and-password-in-python-what-are-my-options
+#
+# BUG: mailbox parser incorrectly parses mail body starting with "From "...
+# example: 403861:From £7* to all London airports one-way...
+# Crashes with:
+# Traceback (most recent call last):
+  # File "<stdin>", line 1, in <module>
+  # File "/usr/lib/python3.6/mailbox.py", line 73, in __getitem__
+    # return self.get_message(key)
+  # File "/usr/lib/python3.6/mailbox.py", line 781, in get_message
+    # msg.set_from(from_line[5:].decode('ascii'))
+# UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 0: ordinal not in range(128)
+# Original exception was:
+# Traceback (most recent call last):
+  # File ".../thunderbird-sort-by-message-count.py", line 98, in <module>
+    # main()
+  # File ".../thunderbird-sort-by-message-count.py", line 75, in main
+    # for idx, em in enumerate(m_box):
+  # File "/usr/lib/python3.6/mailbox.py", line 109, in itervalues
+    # value = self[key]
+  # File "/usr/lib/python3.6/mailbox.py", line 73, in __getitem__
+    # return self.get_message(key)
+  # File "/usr/lib/python3.6/mailbox.py", line 781, in get_message
+    # msg.set_from(from_line[5:].decode('ascii'))
+# UnicodeDecodeError: 'ascii' codec can't decode byte 0xc2 in position 0: ordinal not in range(128)
+# fix unclosed file error:
+# Exception ignored in: <_io.FileIO name='.../INBOX' mode='rb+' closefd=True>
+# ResourceWarning: unclosed file <_io.BufferedRandom name='.../INBOX'>
 
 def main():
   parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -52,12 +79,13 @@ def main():
   
   if args.field is None:
     print('No field specified. Choose one from the following:')
-    m = mailbox.mbox(args.mboxfile[0])
-    em = m[0]
+    m_box = mailbox.mbox(args.mboxfile[0])
+    em = m_box[0]
     print('--------------------------------------------------')
     for i in em.keys():
       print(i)
     print('--------------------------------------------------')
+    m_box.close()
     sys.exit(-1)
   
   mailids = []
@@ -70,13 +98,16 @@ def main():
   
   for mboxfile in args.mboxfile:
     N2 += len(mailbox.mbox(mboxfile))
-    for em in mailbox.mbox(mboxfile):
+    m_box = mailbox.mbox(mboxfile)
+    for idx, em in enumerate(m_box):
+      print('idx = {}'.format(idx))
       N1 += 1
       mailids.append(em[args.field])
       if 'X-Mozilla-Status' in em.keys():
         N_read += 1
       else:
         N_unread += 1
+    m_box.close()
   
   count_dictionary = collections.Counter(mailids)
   count_dictionary_sorted = sorted(count_dictionary, key=lambda k: count_dictionary[k])
