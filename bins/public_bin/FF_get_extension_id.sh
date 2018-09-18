4ce5124f852a7bf63c54b2949a4e9c8104f31661
KIAaze <zohn.joidberg@gmail.com>
2018-09-18 15:21:39 +0100

    made script work for new webAPI addons + added optional global install

--- bins/public_bin/FF_get_extension_id.sh
+++ bins/public_bin/FF_get_extension_id.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Retrieve the extension id for an addon from its install.rdf
+# Retrieve the extension id for a firefox addon from its install.rdf or manifest.json and optionally install it as a global extension.
 # cf http://kb.mozillazine.org/Determine_extension_ID
 
 set -eu
@@ -9,12 +9,45 @@ set -eu
 if [ $# -ne 1 ]
 then
         echo "usage :"
-        echo "`basename $0` arg1"
+        echo "`basename $0` XPI_FILE"
         exit 0
 fi
 
-unzip -qc $1 install.rdf | xmlstarlet sel \
+# old addons:
+if unzip -qc ${1} install.rdf &>/dev/null
+then
+  echo "==> Old non-WebAPI addon:"
+  NAME=$(unzip -qc ${1} install.rdf | xmlstarlet sel \
   -N rdf=http://www.w3.org/1999/02/22-rdf-syntax-ns# \
   -N em=http://www.mozilla.org/2004/em-rdf# \
   -t -v \
-  "//rdf:Description[@about='urn:mozilla:install-manifest']/em:id"
+  "//rdf:Description[@about='urn:mozilla:install-manifest']/em:name")
+  ID=$(unzip -qc ${1} install.rdf | xmlstarlet sel \
+  -N rdf=http://www.w3.org/1999/02/22-rdf-syntax-ns# \
+  -N em=http://www.mozilla.org/2004/em-rdf# \
+  -t -v \
+  "//rdf:Description[@about='urn:mozilla:install-manifest']/em:id")
+else
+  echo "==> New WebAPI addon:"
+  # new WebAPI addons:
+  NAME=$(unzip -qc ${1} manifest.json | jq '.name')
+  ID=$(unzip -qc ${1} manifest.json | jq '.applications.gecko.id')
+  
+  # remove double quotes: https://stackoverflow.com/questions/9733338/shell-script-remove-first-and-last-quote-from-a-variable
+  temp="${NAME%\"}"
+  NAME="${temp#\"}"
+  temp="${ID%\"}"
+  ID="${temp#\"}"
+
+fi
+echo "NAME: ${NAME}"
+echo "ID: ${ID}"
+
+INSTALLDIR=/usr/lib/firefox-addons/extensions
+#INSTALLDIR=${HOME}/.mozilla/extensions/ # Does not seem to work. :(
+
+echo "Install to ${INSTALLDIR} ?"
+read ans
+case $ans in
+  y|Y|yes) sudo cp --interactive --verbose ${1} ${INSTALLDIR}/${ID}.xpi;;
+esac
