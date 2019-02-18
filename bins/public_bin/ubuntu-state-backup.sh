2003ffa0c820aacedb01b9ad4cdad47963949af0
KIAaze <zohn.joidberg@gmail.com>
2019-02-18 19:49:08 +0000

    -pre-upgrade state-backup script added -prompt improved for shell debugging

---
+++ bins/public_bin/ubuntu-state-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# cf: https://askubuntu.com/questions/9135/how-to-backup-settings-and-list-of-installed-packages
+
+set -eux
+
+DSTDIR=${1}
+
+dpkg --list > ${DSTDIR}/dpkg.log
+dpkg --get-selections > ${DSTDIR}/Package.list
+mkdir --parents ${DSTDIR}/etc.apt/
+cp --recursive --verbose /etc/apt/sources.list* ${DSTDIR}/etc.apt/
+apt-key exportall > ${DSTDIR}/Repo.keys
+
+apt-clone clone ${DSTDIR}/apt-clone-state-ubuntu-$(lsb_release -sr)-$(date +%F).tar.gz &> ${DSTDIR}/apt-clone.log
