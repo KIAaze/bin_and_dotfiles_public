9d1ea63c6d189701e90ba6931c3e2987c756b9a3
KIAaze <zohn.joidberg@gmail.com>
2019-06-14 15:20:52 +0100

    WIP

--- bins/public_bin/removeDuplicateDirectories.sh
+++ bins/public_bin/removeDuplicateDirectories.sh
@@ -10,9 +10,30 @@ then
         exit 0
 fi
 
+# if diff --recursive "${1}" "${2}"
+# then
+#   echo rm --recursive "${2}"
+# else
+#   echo "ERROR: Directories are different"
+# fi
+
 if diff --recursive "${1}" "${2}"
 then
-  echo rm --recursive "${2}"
+  echo "diff test: PASSED"
 else
-  echo "ERROR: Directories are "
+  echo "diff test: FAILED"
+fi
+
+if ds-diff.sh "${1}" "${2}"
+then
+  echo "ds-diff.sh test: PASSED"
+else
+  echo "ds-diff.sh test: FAILED"
+fi
+
+if meld "${1}" "${2}"
+then
+  echo "meld test: PASSED"
+else
+  echo "meld test: FAILED"
 fi
