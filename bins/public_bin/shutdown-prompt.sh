0eaf1ac16a8191c7810a28111903d6d1b5a98c07
KIAaze <zohn.joidberg@gmail.com>
2019-07-02 01:33:15 +0100

    -timer set to 1 minute -still not working via crontab due to "Interactive authentication required."

--- bins/public_bin/shutdown-prompt.sh
+++ bins/public_bin/shutdown-prompt.sh
@@ -3,13 +3,14 @@ export DISPLAY=:0
 
 mplayer ${HOME}/Music/non-music/Starcraft_Sounds_ENG/Terran/Ghost/TGhLas00.wav
 
-for ((i=0;i<=100;i++)); do echo $i; sleep 0.1s; done | zenity --progress --auto-close --text="Shutting down in a few seconds..." --title="Automatic shutdown"
+for ((i=0;i<=100;i++)); do echo $i; sleep 0.6s; done | zenity --progress --auto-close --text="Shutting down in a minute now..." --title="Automatic shutdown"
 if test $? -eq 0
 then
   echo "Shutting down"
   /sbin/shutdown -h now
 else
   echo "Cancelling shutdown"
+  /sbin/shutdown -h +65
 fi
 #qdbus org.kde.ksmserver /KSMServer &> /tmp/foo.txt
 #qdbus org.kde.ksmserver /KSMServer org.kde.KSMServerInterface.logout 1 2 3
