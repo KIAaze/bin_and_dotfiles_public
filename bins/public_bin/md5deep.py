9ed658804d206721c7bcc23985b787062204c2d2
KIAaze <zohn.joidberg@gmail.com>
2020-05-04 17:32:24 +0100

    md5deep.py slightly improved.

--- bins/public_bin/md5deep.py
+++ bins/public_bin/md5deep.py
@@ -1,46 +1,81 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
 import os
-
-def usage():
-  print 'usage : '+sys.argv[0]+' DIRECTORY1 DIRECTORY2'
+import argparse
 
 # md5deep options:
 # -r : recursive
 # -o f : only process regular files
-def md5deep_and_strip(dir,file_abs,file_stripped):
-  print 'dir =', dir
+def md5deep_and_strip(directory, file_abs, file_stripped):
+  print( 'directory =', directory)
   
-  print '=== Calculating MD5 for ' + dir + ' ==='
-  cmd='md5deep -r -o f ' + dir + '/* > ' + file_abs
-  print cmd
+  print( '=== Calculating MD5 for ' + directory + ' ===')
+  cmd='md5deep -r -o f ' + directory + '/* > ' + file_abs
+  print(cmd)
   if( os.system(cmd) != 0 ):
     sys.exit(1)
-  
-  print '=== Removing prefix : '+file_abs+'->'+file_stripped+' ==='
+
+  hashdata = []
+  print('=== Removing prefix : '+file_abs+'->'+file_stripped+' ===')
   in_file = open(file_abs,'r')
   out_file = open(file_stripped,'w')
   for line in in_file:
-    #print line
-    stripped_line = line.replace(dir+'/','')
-    #print stripped_line
+    stripped_line = line.replace(directory+'/','')
     out_file.write(stripped_line)
+    fullpath = line.split()[1]
+    hashdata.append(stripped_line.split() + [fullpath])
   in_file.close()
   out_file.close()
+
+  return hashdata
+
+def main():
+  parser = argparse.ArgumentParser(description='Runs md5deep on two directories and compares the results.')
+  parser.add_argument('DIR1')
+  parser.add_argument('DIR2')
+  parser.add_argument('-v', '--verbose', action="count", dest="verbosity", default=0, help='verbosity level')
+  args = parser.parse_args()
+  print(args)
+
+  DIRECTORY_1 = os.path.abspath(args.DIR1)
+  DIRECTORY_2 = os.path.abspath(args.DIR2)
+
+  hashdata1 = md5deep_and_strip(DIRECTORY_1,'md5_1.txt','md5_1_clean.txt')
+  hashdata2 = md5deep_and_strip(DIRECTORY_2,'md5_2.txt','md5_2_clean.txt')
+
+  print('=== DIFFERENCES ===')
+  cmd='diff md5_1_clean.txt md5_2_clean.txt'
+  print(cmd)
+  if( os.system(cmd) != 0 ):
+    differences_found = True
+
+  hash_dict = dict()
+  hash_dict_fullpath = dict()
   
-if len(sys.argv)!=3:
-  usage()
-  sys.exit(0)
+  for h, f, fullpath in hashdata1:
+    if h not in hash_dict.keys():
+      hash_dict[h]=set([(f, fullpath)])
+    else:
+      hash_dict[h].add((f, fullpath))
+      
+  for h, f, fullpath in hashdata2:
+    if h not in hash_dict.keys():
+      hash_dict[h]=set([(f, fullpath)])
+    else:
+      hash_dict[h].add((f, fullpath))
 
-DIRECTORY_1=os.path.abspath(sys.argv[1])
-DIRECTORY_2=os.path.abspath(sys.argv[2])
+  for k,v in hash_dict.items():
+    if len(v) > 1:
+      print('Duplicates found for {}:'.format(k))
+      for i, j in v:
+        print('  {} {}'.format(i, j))
+  
+  if differences_found:
+    sys.exit(1)
+  
+  return 0
 
-md5deep_and_strip(DIRECTORY_1,'md5_1.txt','md5_1_clean.txt')
-md5deep_and_strip(DIRECTORY_2,'md5_2.txt','md5_2_clean.txt')
-
-print '=== DIFFERENCES ==='
-cmd='diff md5_1_clean.txt md5_2_clean.txt'
-print cmd
-if( os.system(cmd) != 0 ):
-  sys.exit(1)
+if __name__ == '__main__':
+  main()
