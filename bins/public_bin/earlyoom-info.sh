f1c8112de820dc23a35929b7cc9e1d1d9d709507
KIAaze <zohn.joidberg@gmail.com>
2021-11-04 09:45:00 +0000

    made faster by saving log to tmp file

--- bins/public_bin/earlyoom-info.sh
+++ bins/public_bin/earlyoom-info.sh
@@ -2,13 +2,16 @@
 # quick info on any processes killed by earlyoom
 
 systemctl --no-pager status earlyoom
+
+JOURNAL_OUTPUT=$(mktemp)
+journalctl -u earlyoom.service >${JOURNAL_OUTPUT}
 echo "==================="
-journalctl -u earlyoom.service | grep -e "SIGTERM to process" -e "SIGKILL to process"
+cat ${JOURNAL_OUTPUT} | grep -e "SIGTERM to process" -e "SIGKILL to process"
 echo "==================="
-PERCENT_MEM_SIGTERM=$(journalctl -u earlyoom.service | grep "SIGTERM when" | tail -1 | perl -n -e'/SIGTERM\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$1"')
-PERCENT_SWAP_SIGTERM=$(journalctl -u earlyoom.service | grep "SIGTERM when" | tail -1 | perl -n -e'/SIGTERM\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$2"')
-PERCENT_MEM_SIGKILL=$(journalctl -u earlyoom.service | grep "SIGKILL when" | tail -1 | perl -n -e'/SIGKILL\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$1"')
-PERCENT_SWAP_SIGKILL=$(journalctl -u earlyoom.service | grep "SIGKILL when" | tail -1 | perl -n -e'/SIGKILL\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$2"')
+PERCENT_MEM_SIGTERM=$(cat ${JOURNAL_OUTPUT} | grep "SIGTERM when" | tail -1 | perl -n -e'/SIGTERM\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$1"')
+PERCENT_SWAP_SIGTERM=$(cat ${JOURNAL_OUTPUT} | grep "SIGTERM when" | tail -1 | perl -n -e'/SIGTERM\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$2"')
+PERCENT_MEM_SIGKILL=$(cat ${JOURNAL_OUTPUT} | grep "SIGKILL when" | tail -1 | perl -n -e'/SIGKILL\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$1"')
+PERCENT_SWAP_SIGKILL=$(cat ${JOURNAL_OUTPUT} | grep "SIGKILL when" | tail -1 | perl -n -e'/SIGKILL\s+when\s+mem\s+<=\s+([-.0-9]*)%\s+and\s+swap\s+<=\s+([-.0-9]*)%/ && print "$2"')
 echo "PERCENT_MEM_SIGTERM=${PERCENT_MEM_SIGTERM}"
 echo "PERCENT_SWAP_SIGTERM=${PERCENT_SWAP_SIGTERM}"
 echo "PERCENT_MEM_SIGKILL=${PERCENT_MEM_SIGKILL}"
