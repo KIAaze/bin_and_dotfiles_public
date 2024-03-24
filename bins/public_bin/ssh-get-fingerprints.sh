609b91bbaa46551091e41becab9787cca686306d
KIAaze <KIAaze@users.noreply.github.com>
2024-03-24 14:00:58 +0000

    -ssh-get-fingerprints.sh now accepts a directory argument to use something else than /etc/ssh. -README updated.

--- bins/public_bin/ssh-get-fingerprints.sh
+++ bins/public_bin/ssh-get-fingerprints.sh
@@ -6,14 +6,19 @@
 # single-line version:
 # for f in /etc/ssh/*.pub; do ssh-keygen -l -f $f; done
 
-for f in /etc/ssh/*.pub
+DIR=${1:-/etc/ssh}
+echo "DIR: ${DIR}"
+
+for f in "${DIR}"/*.pub
 do
+  echo "--> file: ${f}"
 #   ssh-keygen -l -f $f -E md5
-  ssh-keygen -l -f $f
+  ssh-keygen -l -f ${f}
 done
 
-for f in /etc/ssh/*.pub
+for f in "${DIR}"/*.pub
 do
+  echo "--> file: ${f}"
   #ssh-keygen -l -f $f -E sha256
-  awk '{print $2}' $f | base64 -d | sha256sum -b | awk '{print $1}' | xxd -r -p | base64 # (sha256 on old OpenSSH)
+  awk '{print $2}' ${f} | base64 -d | sha256sum -b | awk '{print $1}' | xxd -r -p | base64 # (sha256 on old OpenSSH)
 done
