85131fd4aa6983559ebffcbb5e81e3bb6ead9f11
KIAaze <KIAaze@users.noreply.github.com>
2025-11-08 08:28:11 +0000

    Dsiabling extra custom resizing from autocrop.sh script.

--- bins/public_bin/autocrop.sh
+++ bins/public_bin/autocrop.sh
@@ -23,7 +23,7 @@ do
   # [4, 4] cm
   #convert -resize 480x480 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 480x480 "${outfile}" "${filename}.cropped.4x4.${extension}"
   # 4.375x4.375 cm
-  convert -resize 525x525 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 525x525 "${outfile}" "${filename}.cropped.4.375x4.375.${extension}"
+  #convert -resize 525x525 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 525x525 "${outfile}" "${filename}.cropped.4.375x4.375.${extension}"
   # [5, 5] cm
   #convert -resize 600x600 -density 120x120 -units pixelspercentimeter -background white -gravity center -extent 600x600 "${outfile}" "${filename}.cropped.5x5.${extension}"
 done
